b9b79fc83ef109143ffd6124e2fe61525912132b
nihad <nihadbaba.2800@gmail.com>
2023-11-19 09:46:32 -0500

    my first commit

---
+++ src/test/java/com/loop/test/day1_selenium_intro/SeleniumTest.java
@@ -0,0 +1,35 @@
+package com.loop.test.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) throws InterruptedException {
+        // setting up the web driver
+        WebDriverManager.chromedriver().setup();
+        // create an instance of web diver
+        WebDriver driver = new ChromeDriver();
+       // Thread.sleep(5000);
+        // navigate to web page
+      //  driver.get("https://www.loopcamp.io");
+       driver.navigate().to("https://www.google.com/");
+       // driver.manage().window().maximize();
+        driver.manage().window().fullscreen();
+
+        driver.navigate().to("https://www.etsy.com");
+
+        // navigate back
+        driver.navigate().back();
+
+        // navigate forward
+        driver.navigate().forward();
+
+        // navigate refresh
+        driver.navigate().refresh();
+
+        // quit close
+        driver.quit();
+
+    }
+}
